5827c7a67c7d1faf346c13d537b2ade7f9ff59ee
Mocca101 <moritz.grossfurtner@protonmail.com>
2019-04-18 20:01:58 +0200

    - UnityInputsystem removed + Sinput + Super basic MovementScript + Started Work on InputManager

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] FloatReference movementSpeed = new FloatReference(10);
+    [SerializeField] Rigidbody2D movingObject;
+
+    public Rigidbody2D MovingObject { private get => movingObject; set => movingObject = value; }
+
+    public void move(Vector2 direction)
+    {
+        MovingObject.velocity = direction *movementSpeed;
+    }
+
+}
