728df4d67e1f6eec8bfcafe200bfa70d05aa51a6
Mario Kerndler <mariokerndler@gmail.com>
2019-04-18 16:50:00 +0200

    Added IDamageable interface

--- Assets/Scripts/Classes/Test.cs
+++ Assets/Scripts/Classes/Test.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using PlayableCharacters;
+using Characters;
 
 public class Test : MonoBehaviour
 {
---
+++ Assets/Scripts/Interfaces/IDamagable.cs
@@ -0,0 +1,7 @@
+﻿namespace Characters {
+    public interface IDamagable 
+    {
+        void getDamage(LivinEntity damageDealer, float damage);
+        void die(LivinEntity damageDealer);
+    }
+}
