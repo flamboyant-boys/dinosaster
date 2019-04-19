7c1f779ac358748856a378ce9ac794e128ccecc1
Mario Kerndler <mariokerndler@gmail.com>
2019-04-19 17:33:57 +0200

    UI

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,37 +4,64 @@ using UnityEngine;
 using UnityEngine.UI;
 using Characters;
 using System;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
-    public List<Player> players = new List<Player>();
+    private List<Player> players = new List<Player>();
+    public GameObject uiPrefab;
+    public Transform uiParent;
+    public int maxStock = 3;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetPlayers();
+    }
+
+    public void GetPlayers()
+    {
+        LivingEntity[] livingEntities = GameObject.FindObjectsOfType<LivingEntity>();
+        foreach(LivingEntity ent in livingEntities)
+        {
+            GameObject uiInstance = Instantiate(uiPrefab, uiParent);
+            uiInstance.name = ent.Name + "_UI";
+            players.Add(new Player(ent.gameObject, maxStock, uiInstance));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach(Player p in players)
+        {
+            p.UpdateStats();
+        }
     }
 }
 
 [Serializable]
 public struct Player
 {
-    public LivingEntity character;
+    public GameObject characterPrefab;
     public int stock;
     public GameObject ui;
 
-    public Player(LivingEntity character, int stock, GameObject ui)
+    private LivingEntity character;
+    private TextMeshProUGUI text;
+    private Image image;
+
+    public Player(GameObject characterPrefab, int stock, GameObject ui)
     {
-        this.character = character;
+        this.characterPrefab = characterPrefab;
         this.stock = stock;
         this.ui = ui;
+
+        character = characterPrefab.GetComponent<LivingEntity>();
+        text = ui.GetComponentInChildren<TextMeshProUGUI>();
+        image = ui.GetComponentInChildren<Image>();
     }
+
 
     public int Stock {
         get { return stock; }
@@ -50,7 +77,7 @@ public struct Player
 
     public void UpdateStats()
     {
-        TextMesh text = ui.GetComponentInChildren<TextMesh>();
+        text.text = character.Percentage.ToString() + "%";
     }
 
     public bool decreaseStock(int ammount)
